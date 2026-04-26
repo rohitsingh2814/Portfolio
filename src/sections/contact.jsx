6ac0c241e81daf54ddcfb6980fd9fd75c708f2ed
Rohit Singh <rohitsingh7667142299@gmail.com>
2026-04-26 10:31:29 +0530

    working on contact section

--- src/sections/contact.jsx
+++ src/sections/contact.jsx
@@ -1,5 +1,72 @@
-export const Contact=()=>{
-    return<section>
-        
-    </section>
+import { Mail, Phone, MapPin } from "lucide-react"
+const contactInfo = [{
+    icon: Mail,
+    label: "Email",
+    value: "rohitsingh7667142299@gmail.com",
+    href: "mailto:rohitsingh7667142299@gmail.com",
+},
+{
+    icon: Phone,
+    label: "Phone",
+    value: "+91 7667142299",
+    href: "tel:+917667142299",
+},
+{
+    icon: MapPin,
+    label: "Location",
+    value: "Ranchi ,JH",
+    href: "#",
+}
+];
+
+
+
+export const Contact = () => {
+    return (
+        <section id="projects" className="py-32 relative overflow-hidden">
+            <div className="absolute top-1/4 right-0 w-96 h-96 bg-primary/5 rounded-full blur-3xl" />
+            <div className="absolute bottom-1/4 left-0 w-64 h-64 bg-highlight/5 rounded-full blur-3xl" />
+
+            <div className="container mx-auto px-6 relative z-10">
+                {/* Section Header */}
+                <div className="text-center mx-auto max-w-3xl mb-16">
+                    <span className="text-secondary-foreground text-sm font-medium tracking-wider uppercase animate-fade-in">
+                        Get In Touch
+                    </span>
+                    <h2 className="text-4xl md:text-5xl font-bold mt-4 mb-6 animate-fade-in animation-delay-100 text-secondary-foreground">
+                        Let's build{" "}
+                        <span className="font-serif italic font-normal text-white">
+                            something great
+
+                        </span>
+                    </h2>
+                    <p className="text-muted-foreground animate-fade-in animation-delay-200">
+                        Have a project in mind? I'd love to hear about it. send me a message
+                        and let's discuss how we can work together.
+                    </p>
+                </div>
+
+                <div className="grid lg:grid-cols-2 gap-12 max-w-5xl mx-auto">
+                    <div className="glass p-8 rounded-3xl border border-primary/30 animate-fade-in  animation-300">
+                        <form action="">
+                            <div>
+                                <label htmlFor="name" className="block text-sm font-medium md-2">Name</label>
+                                <input id="name" type="text" className="w-full px-4 py-3 bg-surface rounded-xl border border-border focus:border-primary focus:ring-1 focus:ring-primary outline-none transition-all"/>
+                            </div>
+
+                            <div>
+                                 <label htmlFor="email" className="block text-sm font-medium md-2">Email</label>
+                                 <input id="email" type="email" className="w-full px-4 py-3 bg-surface rounded-xl border border-border focus:border-primary focus:ring-1 focus:ring-primary outline-none transition-all"/>
+                            </div>
+
+                            <div>
+                            <label htmlFor="message" className="block text-sm font-medium md-2">Message</label>
+                             <text id="message"  className="w-full px-4 py-3 bg-surface rounded-xl border border-border focus:border-primary focus:ring-1 focus:ring-primary outline-none transition-all resize-none"/>
+                           </div>
+                       </form>
+            </div>
+        </div>
+      </div >
+         </section >
+    )
 };
